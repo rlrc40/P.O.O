0f7b72de6d9adfdbfe313dc3cb42406fc2b5a5c7
Raúl Rodríguez Carracedo <raul.rodriguez.carracedo@alumnos.uva.es>
2017-01-12 14:05:12 +0100

    Delete PruebaReloj.java

--- PruebaReloj.java
+++
@@ -1,8 +0,0 @@
-package ejercicios_POO;
-
-public class PruebaReloj {
-	public static void main (String [] args) throws InterruptedException{
-		Reloj miReloj = new Reloj(23,59,55);
-		miReloj.run();
-	}
-}
